363642b59a1af96eda3b604df2f84e59aa3c572e
Gabber235 <gabber200@icloud.com>
2021-04-25 13:46:17 +0200

    Sub2 done

--- src/objects/submarine/stage3.js
+++ src/objects/submarine/stage3.js
@@ -8,8 +8,8 @@ export default class SubmarineStage3 extends Submarine {
 		this.obj = this.scene.add.rectangle(this.initX, this.initY, this.width, this.height, this.color);
 
 		this.weapons = [
-			new Cannon({scene, submarine: this, relativeX: 60, relativeY: 130, defaultAngle: 90, minAngle: 60, maxAngle: 150}),
-			new Cannon({scene, submarine: this, relativeX: 190, relativeY: 130, defaultAngle: 90, minAngle: 30, maxAngle: 120})
+			// new Cannon({scene, submarine: this, relativeX: 60, relativeY: 130, defaultAngle: 90, minAngle: 60, maxAngle: 150}),
+			// new Cannon({scene, submarine: this, relativeX: 190, relativeY: 130, defaultAngle: 90, minAngle: 30, maxAngle: 120})
 		]
 
 		this.init();
